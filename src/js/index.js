34ce1dc0bc2a88f71a03bd80bb880487c42c7911
MykhailoYavorskyi <my70035@gmail.com>
2023-08-11 14:48:49 +0200

    start

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+const refs = {
+  select: document.querySelector('.breed-select'),
+  loaderText: document.querySelector('.loader'),
+  errorText: document.querySelector('.error'),
+  box: document.querySelector('.cat-info'),
+};
