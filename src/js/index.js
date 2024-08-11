68ae538f411ed17a23a12ac42b31448fefa003ad
MykhailoYavorskyi <my70035@gmail.com>
2024-08-11 11:24:02 +0200

    add

--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-const refs = {
-  select: document.querySelector('.breed-select'),
-  loaderText: document.querySelector('.loader'),
-  errorText: document.querySelector('.error'),
-  box: document.querySelector('.cat-info'),
-};
